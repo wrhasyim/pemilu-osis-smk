476e6f107958eea49aa13ffaedd159d0ced99294
wrhasyim <wrhasyim98@gmail.com>
2025-10-09 12:41:05 +0700

    Refactor code structure for improved readability and maintainability

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0', // <-- TAMBAHKAN BARIS INI
+        hmr: {
+            host: 'localhost' // Ini penting agar HMR tetap berjalan
+        }
+    }
 });
